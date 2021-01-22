4075fa92cbd85d00b0394e4cd24bb79d60f08c65
Freewind <nowindlee@gmail.com>
2021-01-22 21:13:26 +0800

    not sure why refreshing is broken

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,6 +1,3 @@
-// Snowpack Configuration File
-// See all supported options: https://www.snowpack.dev/reference/configuration
-
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
@@ -10,6 +7,14 @@ module.exports = {
   plugins: [
     '@snowpack/plugin-react-refresh'
   ],
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+  ],
+  optimize: {
+    /* Example: Bundle your final build: */
+    // "bundle": true,
+  },
   packageOptions: {
     /* ... */
   },
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { Hello } from './hello'
+import {Hello} from './hello'
 
 ReactDOM.render(
   <Hello/>,
-  document.body
+  document.getElementById('root')
 )
